e7667a573cdc5c1fe6b9131838f7ed5938b80f6f
juriguss <juriguss@greeny.cs.tlu.ee>
2016-09-06 15:17:43 +0300

    first.php

---
+++ first.php
@@ -0,0 +1,19 @@
+
+
+
+<?php
+
+	$firstName = "Juri";
+	$lastName = "Gussarov";
+	
+	// echo trukib valja muutuja veebilehel
+	
+	//echo $firstName;
+	//echo " ";
+	//echo $lastName;
+	
+	// punkt lidab srtenge
+
+	echo $firstName." ".$lastName;
+	
+?>
